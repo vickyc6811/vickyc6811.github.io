cd6ec762f3ff246ae4453baf79cc3b38c0a7925e
vickyc6811 <73483037+vickyc6811@users.noreply.github.com>
2020-10-26 12:16:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Vicky Chen"
+let userName = "vickyc6811";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
